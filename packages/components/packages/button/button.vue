52f16b5043b3031f0f9e63f3257cd7d178afeaa8
张文皓 <zhangwh754@163.com>
2023-10-07 23:39:40 +0800

    按钮组件样式添加

--- packages/components/packages/button/button.vue
+++ packages/components/packages/button/button.vue
@@ -1,7 +1,11 @@
 <template>
-  <button>我的按钮23</button>
+  <button class="wh-button wh-button--primary">我的按钮23</button>
 </template>
 
 <script lang="ts" setup>
 defineOptions({ name: "wh-button" });
 </script>
+
+<style lang="less" scoped>
+@import "./style.less";
+</style>
