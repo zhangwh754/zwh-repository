c8e55807d07e29f1f794233329533f460df3b813
张文皓 <zhangwh754@163.com>
2023-10-08 11:31:21 +0800

    按钮组件动态传值
    1、动态传递按钮类型，应用不同的类名
    2、动态传递按钮文字，应用不同的文字

--- live-play/App.vue
+++ live-play/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div>启动测试</div>
 
-  <wh-button @click="easyLog"></wh-button>
+  <wh-button type="default" label="你好" @click="easyLog">default</wh-button>
+  <wh-button type="primary" @click="easyLog">primary</wh-button>
+  <wh-button type="warning" @click="easyLog">warning</wh-button>
 </template>
 
 <script lang="ts" setup>
--- packages/components/packages/button/button.vue
+++ packages/components/packages/button/button.vue
@@ -1,9 +1,30 @@
 <template>
-  <button class="wh-button wh-button--primary">我的按钮23</button>
+  <button :class="['wh-button', buttonClassName]">{{ props.label }}</button>
 </template>
 
 <script lang="ts" setup>
+import { computed } from "vue";
+
 defineOptions({ name: "wh-button" });
+
+const props = withDefaults(
+  defineProps<{
+    type?: "primary" | "default" | "warning";
+    label?: string;
+  }>(),
+  {
+    type: "default",
+    label: "默认文字",
+  }
+);
+
+const buttonClassName = computed(() => {
+  const name = ["primary", "default", "warning"].includes(props.type)
+    ? props.type
+    : "default";
+
+  return `wh-button--${name}`;
+});
 </script>
 
 <style lang="less" scoped>
