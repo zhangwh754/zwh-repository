12f3df2e7e4601932681434d87aaa07a2b3cda92
张文皓 <zhangwh754@163.com>
2023-10-10 15:10:07 +0800

    本地安装npm @djdg626/components_test组件库
    1、pnpm-workspace排除本地打包后的组件
    2、全局引入WhButton组件

--- live-play/main.ts
+++ live-play/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from "vue";
 // import { Button } from "@zwh/components";
-import zwhComponents from "@zwh/components";
+import zwhComponents from "@djdg626/components_test";
+// import zwhComponents from "@zwh/components";
 import App from "./App.vue";
 
 const app = createApp(App);
